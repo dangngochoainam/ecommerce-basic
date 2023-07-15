4855bba60da10a7ca8a56ee76778aac46fd7c606
Hoai Nam <nam.dang@bnksolution.com>
2023-07-15 21:51:55 +0700

    section 9 - login

--- src/controllers/access.controller.js
+++ src/controllers/access.controller.js
@@ -1,9 +1,16 @@
 'use strict';
 
-const { CREATED } = require('../core/success.response');
+const { CREATED, OK, SuccessResponse } = require('../core/success.response');
 const AccessService = require('../services/access.service');
 
 class AccessController {
+  login = async (req, res, next) => {
+    return new SuccessResponse({
+      message: 'Login successfully',
+      metadata: await AccessService.login(req.body),
+    }).send(res);
+  };
+
   signUp = async (req, res, next) => {
     return new CREATED({
       metadata: await AccessService.signUp(req.body),
