3cf38b0f89ba78f737e2410e1d331751fcc74bf3
Hoai Nam <nam.dang@bnksolution.com>
2023-08-12 17:34:27 +0700

    section 20 - model discount

---
+++ src/models/discount.model.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+const DOCUMENT_NAME = 'Discount';
+const COLLECTION_NAME = 'discounts';
+
+const discountSchema = new Schema(
+  {
+    discount_name: { type: String, require: true },
+    discount_description: { type: String, require: true },
+    discount_type: { type: String, default: 'fixed_amount' },
+    discount_value: { type: Number, require: true },
+    discount_start_date: { type: Date, require: true },
+    discount_end_date: { type: Date, require: true },
+    discount_max_uses: { type: Number, require: true }, // so lượng discount được sử dụng
+    discount_uses_count: { type: Number, require: true }, // so lượng discount đã sử dụng
+    discount_users_used: { type: Array, default: [] }, // ai đã sử dụng
+    discount_max_uses_per_user: { type: Number, require: true }, // so lượng cho phép toi da 1 người có thể sử dụng discount này
+    discount_min_order_value: { type: Number, require: true },
+    discount_shopId: { type: Schema.Types.ObjectId, ref: 'Shop' },
+
+    discount_is_active: { type: Boolean, default: true },
+    discount_applies_to: {
+      type: String,
+      require: true,
+      enum: ['all', 'specific'],
+    },
+    discount_product_ids: { type: Array, default: [] }, // so san pham được áp dụng
+  },
+  {
+    timestamps: true,
+    collection: COLLECTION_NAME,
+  }
+);
+
+module.exports = {
+  discount: model(DOCUMENT_NAME, discountSchema),
+};
